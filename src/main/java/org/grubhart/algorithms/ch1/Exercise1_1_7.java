c058ac9aea49d7387de365811afe8dfc930a5e41
grubhart <grubhart@gmail.com>
2019-01-14 11:01:12 -0500

    add more exercises: 1.1.17 - 1.1.18

--- src/main/java/org/grubhart/algorithms/ch1/Exercise1_1_7.java
+++ src/main/java/org/grubhart/algorithms/ch1/Exercise1_1_7.java
@@ -2,10 +2,35 @@ package org.grubhart.algorithms.ch1;
 
 public class Exercise1_1_7 {
     public static void main(String[] args) {
+
+        System.out.println("a:");
         double t=9.0;
         while(Math.abs(t-9.0/t)>0.01){
             t=(9.0/t+t)/2.0;
             System.out.printf("%.5f\n",t);
         }
+
+
+        System.out.println("b:");
+        int sum=0;
+        for (int i=0;i<1000;i++) {
+            for(int j=1;j<i;j++){
+                sum++;
+            }
+        }
+        System.out.println(sum);
+
+        System.out.println("c:");
+
+        sum=0;
+
+        for (int i=1;i<1000;i*=2) {
+            for (int j=0; j<1000; j++) {
+                sum++;
+            }
+        }
+
+        System.out.println(sum);
+
     }
 }
---
+++ src/main/java/org/grubhart/algorithms/ch1/Exercise1_1_8.java
@@ -0,0 +1,11 @@
+package org.grubhart.algorithms.ch1;
+
+public class Exercise1_1_8 {
+
+    public static void main(String[] args) {
+        System.out.println('b');
+        System.out.println('b'+'c');
+        System.out.println((char)('a'+4));
+    }
+
+}
